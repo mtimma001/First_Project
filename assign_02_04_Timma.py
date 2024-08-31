bb3a4cc798ada77a386cb0122a8ad6a0666052c4
Timma <timma28@gmail.com>
2024-08-31 14:17:10 +0300

    last file

---
+++ assign_02_04_Timma.py
@@ -0,0 +1,18 @@
+#this program will collect data from a patient visiting the healthcare center
+
+def patient_register(name, residence, sex, age):
+    #register the patient's name
+    print(f"What is your name? {name}")
+
+    #get the patient's residence
+    print(f"where do you live? {residence}")
+
+    #Get patient's gender
+    print(f"are you male or female? {sex}")
+
+    #get patient's age
+    print(f"How old are you? {age}")
+
+patient_register("Michele Timma", "Texas", "Female", "32")
+
+
